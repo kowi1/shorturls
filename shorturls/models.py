8c70f632030a1276ce34779d0708498f7fcbfb52
DESKTOP-F5VG5HT\OLUKOWI <dolukowi@gmail.com>
2018-10-25 17:02:48 +0100

    changes

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,7 +1,7 @@
 from django.db.models import Model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from shorturls.validators import FriendValidator
+from .validators import FriendValidator
 
 friendlyname_validator=FriendValidator
 
--- shorturls/validators.py
+++ shorturls/validators.py
@@ -1,8 +1,10 @@
 from django.core import validators
+from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
+@deconstructible
 class FriendValidator(validators.RegexValidator):
-    regex = r'^[\w]+$'
+    regex = r'^[a-zA-Z0-9]+$'
     message = _(
         'Enter a Friendly Name. This value may contain only letters,and '
         'numbers'
